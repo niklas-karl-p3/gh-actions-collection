ef17492469df6c1c8fe173c212b6e08e04cdd57d
Niklas Karl <niklas.karl@p3-group.com>
2024-07-16 17:11:51 +0200

    Update build logs.

--- action-1/build.js
+++ action-1/build.js
@@ -1,2 +1,21 @@
-var exec = require('child_process').exec;
-exec('npm run build');
+const core = require('@actions/core');
+const child_process = require('child_process');
+
+try {
+    const stdout = child_process.execSync('npm run build', {
+        stdio: 'pipe',
+        encoding: 'utf8'
+    });
+
+    core.info(stdout);
+} catch (err) {
+    if (err.code) {
+        core.error(`task failed with error code ${err.code}`);
+        core.setFailed(`task failed with error code ${err.code}`);
+    } else {
+        const { stdout, stderr } = err;
+        core.error(`task failed with\nstdout:\n\n${stdout}\n\nstderr:${stderr}`);
+        core.setFailed('task failed.');
+    }
+
+}
